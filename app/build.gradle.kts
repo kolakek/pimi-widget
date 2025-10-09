c85c251e0665f0e6e09ef105a9b35d6b1d7f6d43
kolakek <223337555+kolakek@users.noreply.github.com>
2025-10-09 21:07:03 +0200

    Downgrade to AGP 8.11.1 for F-Droid compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kolakek.pimiwidget"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
