b09b64ef87ef8c2ba8ee137bcbb851a8ce3fca73
kolakek <223337555+kolakek@users.noreply.github.com>
2025-10-12 19:58:31 +0200

    Adjust version number to v1.1.0, add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kolakek.pimiwidget"
         minSdk = 33
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
