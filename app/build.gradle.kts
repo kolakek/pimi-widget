e5427c767e9af1311f4960f17de1913561179172
kolakek <223337555+kolakek@users.noreply.github.com>
2025-11-15 14:08:00 +0100

    Increase version number, add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kolakek.pimiwidget"
         minSdk = 33
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.2.2"
+        versionCode = 9
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
