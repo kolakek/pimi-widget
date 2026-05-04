80e79f9a13d2c4062cd7560571eae47c8db4b0ce
kolakek <223337555+kolakek@users.noreply.github.com>
2026-05-04 22:56:26 +0200

    Increase version number, add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kolakek.pimiwidget"
         minSdk = 33
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.8.0"
+        versionCode = 18
+        versionName = "1.9.0"
     }
     dependenciesInfo {
         includeInApk = false
