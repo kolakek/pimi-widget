896c6d45fb7aab461861db56a69394ee9192ff81
kolakek <223337555+kolakek@users.noreply.github.com>
2026-06-28 22:24:56 +0200

    Support more weather apps

--- app/src/main/java/com/kolakek/pimiwidget/utility/WeatherApp.kt
+++ app/src/main/java/com/kolakek/pimiwidget/utility/WeatherApp.kt
@@ -25,12 +25,62 @@ enum class WeatherApp(val key: String, val packageName: String, val labelId: Int
         "",
         R.string.config_weather_app_default
     ),
-    BREEZY_WEATHER(
+    BREEZY(
         "weather_app_breezy",
         "org.breezyweather",
         R.string.config_weather_app_breezy
     ),
-    PIXEL_WEATHER(
+    METEOFR(
+        "weather_app_meteofr",
+        "fr.meteo",
+        R.string.config_weather_app_meteofr
+    ),
+    KNMI(
+        "weather_app_knmi",
+        "nl.knmi.weer",
+        R.string.config_weather_app_knmi
+    ),
+    DWD(
+        "weather_app_dwd",
+        "de.dwd.warnapp",
+        R.string.config_weather_app_dwd
+    ),
+    METOFF(
+        "weather_app_metoffice",
+        "uk.gov.metoffice.weather.android",
+        R.string.config_weather_app_metoff
+    ),
+    AEMET(
+        "weather_app_aemet",
+        "es.aemet",
+        R.string.config_weather_app_aemet
+    ),
+    METEOSWISS(
+        "weather_app_meteoswiss",
+        "ch.admin.meteoswiss",
+        R.string.config_weather_app_meteoswiss
+    ),
+    YR(
+        "weather_app_yr",
+        "no.nrk.yr",
+        R.string.config_weather_app_yr
+    ),
+    BOM(
+        "weather_app_bom",
+        "au.gov.bom.metview",
+        R.string.config_weather_app_bom
+    ),
+    CAN(
+        "weather_app_can",
+        "ca.gc.ec.weather_app_android.ops",
+        R.string.config_weather_app_can
+    ),
+    DMI(
+        "weather_app_dmi",
+        "dk.dmi.byvejret",
+        R.string.config_weather_dmi
+    ),
+    PIXEL(
         "weather_app_pixel",
         "com.google.android.apps.weather",
         R.string.config_weather_app_pixel
